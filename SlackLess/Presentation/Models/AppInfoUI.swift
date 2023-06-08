21ec4da4d698842fad1189bd9804eaa8d5cb5010
Daniyar <daniyar.kbv@gmail.com>
2023-06-07 21:24:33 -0400

    feat: AppInfoUI

---
+++ SlackLess/Presentation/Models/AppInfoUI.swift
@@ -0,0 +1,15 @@
+//
+//  AppInfoUI.swift
+//  SlackLess
+//
+//  Created by Daniyar Kurmanbayev on 2023-06-07.
+//
+
+import Foundation
+import UIKit
+
+struct AppInfoUI {
+    let name: String
+    let image: UIImage
+    let time: Int
+}
