fe64b29bb835fa7f26ea39e5189f1d15c7088cae
Daniyar <daniyar.kbv@gmail.com>
2023-06-07 21:13:28 -0400

    feat: init

---
+++ SlackLess/Presentation/Features/Summary/Summary/SLAppsCollectionViewController/SLAppsCollectionViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  SLAppsCollectionViewModel.swift
+//  SlackLess
+//
+//  Created by Daniyar Kurmanbayev on 2023-06-07.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+protocol SLAppsCollectionViewModelInput {
+    func terminate()
+}
+
+protocol SLAppsCollectionViewModelOutput {
+    var didFinish: PublishRelay<Void> { get }
+}
+
+protocol SLAppsCollectionViewModel: AnyObject {
+    var input: SLAppsCollectionViewModelInput { get }
+    var output: SLAppsCollectionViewModelOutput { get }
+}
+
+final class SLAppsCollectionViewModelImpl: SLAppsCollectionViewModel, SLAppsCollectionViewModelInput, SLAppsCollectionViewModelOutput {
+    var input: SLAppsCollectionViewModelInput { self }
+    var output: SLAppsCollectionViewModelOutput { self }
+    
+    //    Output
+    var didFinish: PublishRelay<Void> = .init()
+    
+    //    Input
+    func terminate() {
+        didFinish.accept(())
+    }
+}
