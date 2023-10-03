d77aeb82109080f7ec5e652c75d65211ff4ac56d
Daniyar <daniyar.kbv@gmail.com>
2023-10-02 20:27:25 -0400

    feat: terms and conditions made commin

--- SlackLess/Presentation/Features/Onboarding/WelcomeScreen/WelcomeScreenView.swift
+++ SlackLess/Presentation/Features/Onboarding/WelcomeScreen/WelcomeScreenView.swift
@@ -62,51 +62,7 @@ final class WelcomeScreenView: SLView {
         let view = UILabel()
         view.numberOfLines = 0
         view.textAlignment = .center
-
-        let baseAttriibutes: [NSAttributedString.Key: Any] = [
-            .foregroundColor: SLColors.white.getColor() as Any,
-            .font: SLFonts.primary.getFont(ofSize: 13, weight: .regular),
-        ]
-        let accentAttributes: [NSAttributedString.Key: Any] = [
-            .foregroundColor: SLColors.black.getColor() as Any,
-            .underlineStyle: NSUnderlineStyle.single.rawValue,
-            .underlineColor: SLColors.black.getColor() as Any,
-        ]
-
-        var text1 = NSMutableAttributedString(
-            string: SLTexts.WelcomeScreen.termsAndPrivacy1.localized(),
-            attributes: baseAttriibutes
-        )
-        var text2 = NSMutableAttributedString(
-            string: SLTexts.WelcomeScreen.termsAndPrivacy2.localized(),
-            attributes: baseAttriibutes
-        )
-        var text3 = NSMutableAttributedString(
-            string: SLTexts.WelcomeScreen.termsAndPrivacy3.localized(),
-            attributes: baseAttriibutes
-        )
-        var text4 = NSMutableAttributedString(
-            string: SLTexts.WelcomeScreen.termsAndPrivacy4.localized(),
-            attributes: baseAttriibutes
-        )
-        let space = NSAttributedString(string: " ")
-        let newLine = NSAttributedString(string: "\n")
-
-        let text2Range = NSRange(location: 0, length: text2.string.count)
-        let text4Range = NSRange(location: 0, length: text4.string.count)
-
-        text2.addAttributes(accentAttributes, range: text2Range)
-        text4.addAttributes(accentAttributes, range: text4Range)
-
-        text1.append(newLine)
-        text1.append(text2)
-        text1.append(space)
-        text1.append(text3)
-        text1.append(space)
-        text1.append(text4)
-
-        view.attributedText = text1
-
+        view.attributedText = SLTexts.Common.TermsAndPrivacy.makeText(clickElementName: mainButton.titleLabel?.text ?? "")
         return view
     }()
 
