48db7aa3bd5426d7316ab08245a64f867fc8f831
Daniyar <daniyar.kbv@gmail.com>
2024-01-10 23:18:18 -0500

    fix: progress infinite loading on first install

--- SLActivityReport/Presentaion/Common/Models/ARProgress.swift
+++ SLActivityReport/Presentaion/Common/Models/ARProgress.swift
@@ -29,10 +29,7 @@ struct ARProgress {
                                         repository: repository) })
             .unwrap()
         
-        guard days.count > 1
-        else {
-            return nil
-        }
+        guard !days.isEmpty else { return nil }
 
         var weeks = [ARWeek]()
         var currentDate = Date().getFirstDayOfWeek().add(.weekOfYear, value: -4)
